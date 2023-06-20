c691b92a09b553789101f232d4d0289f33555a3f
KIIT <siddhantupadhyay6@gmail.com>
2023-06-20 12:57:34 +0530

    Revised Assignment of vehicle

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,13 +4,34 @@ public class Boat implements WaterVehicle{
   private String name;
   private int capacity;
 
+  public Boat(String name, int capacity) {
+    this.name = name;
+    this.capacity = capacity;
+  }
 
-
-  public String getVehicleName(){
+  public String getName() {
     return name;
   }
 
-  public int getVehicleCapacity(){
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getCapacity() {
     return capacity;
+  }
+
+  public void setCapacity(int capacity) {
+    this.capacity = capacity;
+  }
+
+  @Override
+  public String getVehicleName() {
+    return this.name;
+  }
+
+  @Override
+  public int getVehicleCapacity() {
+    return this.capacity;
   }
 }
